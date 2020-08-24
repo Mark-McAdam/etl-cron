a1a8c7409ac51e0aa944c9e48032908560dcd1bb
Mark McAdam <mark.a.mcadam@gmail.com>
2020-08-24 15:46:40 -0700

    Update 3-deploy.sh

--- 3-deploy.sh
+++ 3-deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# deploy the cloudformation 
 set -eo pipefail
 ARTIFACT_BUCKET=$(cat bucket-name.txt)
 aws cloudformation package --template-file template.yml --s3-bucket $ARTIFACT_BUCKET --output-template-file out.yml
