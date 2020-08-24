e4a32ae49852d866396a70481c35f722bf5f8756
Mark McAdam <mark.a.mcadam@gmail.com>
2020-08-24 15:45:47 -0700

    Update 1-create-bucket.sh

--- 1-create-bucket.sh
+++ 1-create-bucket.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# create an s3 bucket to store resources
+# output bucket name to bucket-name.txt
 BUCKET_ID=$(dd if=/dev/random bs=8 count=1 2>/dev/null | od -An -tx1 | tr -d ' \t\n')
 BUCKET_NAME=kondoboard-etl-cron-artifacts-$BUCKET_ID
 echo $BUCKET_NAME > bucket-name.txt
